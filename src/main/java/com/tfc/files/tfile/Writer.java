fe0c4522754ac9a681690171f2890ba2a3c41249
GiantLuigi4 <tjmferet@hotmail.com>
2020-09-04 21:43:28 -0400

    More javadocs

--- src/main/java/com/tfc/files/tfile/Writer.java
+++ src/main/java/com/tfc/files/tfile/Writer.java
@@ -3,7 +3,16 @@ package com.tfc.files.tfile;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+/**
+ * A helper class for writng TFiles to disk
+ */
 public class Writer {
+	/**
+	 * write a TFile to disk
+	 * @param out the file to write
+	 * @param dir the file to write it to
+	 * @throws IOException if something goes wrong while writing it, IE, the  file doesn't exist
+	 */
 	public static void write(TFile out, String dir) throws IOException {
 		String fileString = out.toString();
 		FileOutputStream stream = new FileOutputStream(dir);
