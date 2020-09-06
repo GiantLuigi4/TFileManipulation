285aa97b8624bd72dab2378e5027828cde9224ef
GiantLuigi4 <tjmferet@hotmail.com>
2020-09-05 20:37:03 -0400

    Two types of number compression

--- src/main/java/com/tfc/files/compression/Numbers.java
+++ src/main/java/com/tfc/files/compression/Numbers.java
@@ -5,7 +5,8 @@ package com.tfc.files.compression;
  */
 public class Numbers {
 	//this makes it so that the result is larger than the initial value
-	private static final int amp = 128;
+	private static final int ampM = 128;
+	private static final int ampD = 64;
 	
 	/**
 	 * helper method for amplifying a value so that it's larger than it should be
@@ -16,7 +17,7 @@ public class Numbers {
 	 * @return amplified result of multiplying those two values together
 	 */
 	private static long amp(long i, long i1) {
-		return (((i * amp) + (i1 * amp)) * amp);
+		return (((i * ampM) + (i1 * ampM)) * ampM);
 	}
 	
 	/**
@@ -30,8 +31,8 @@ public class Numbers {
 		String out = toCompress;
 		boolean replaced = true;
 		while (replaced) {
-			String oldStr = "";
-			String newStr = "";
+			String oldStr = "23432649327492";
+			String newStr = "2347323247324";
 			replaced = false;
 			for (long i = 2; i < 2560; i++) {
 				for (long i1 = 2; i1 < 2560; i1++) {
@@ -39,9 +40,11 @@ public class Numbers {
 					String newS = (((char) 1) + "" + i + "*" + i1 + "" + ((char) 1));
 					if (out.contains(oldS)) {
 						if (oldS.length() > newS.length()) {
-							oldStr = oldS;
-							newStr = newS;
-							replaced = true;
+							if ((oldStr+newStr).length() > (oldS+newS).length()) {
+								oldStr = oldS;
+								newStr = newS;
+								replaced = true;
+							}
 						}
 					}
 				}
@@ -54,12 +57,50 @@ public class Numbers {
 	}
 	
 	/**
-	 * decompress a string that has been compressed using the Number compression
+	 * compress a string that contains numbers using division
+	 * this is a very ineffective method, but it's better than nothing
+	 *
+	 * @param toCompress the string to compress
+	 * @return the compressed string
+	 */
+	public static String compressDivision(String toCompress) {
+		String out = toCompress;
+		boolean replaced = true;
+		while (replaced) {
+			String oldStr = "23432649327492";
+			String newStr = "2347323247324";
+			replaced = false;
+			for (long i = 1; i < 2560; i++) {
+				for (long i1 = 3; i1 < 2560; i1++) {
+					String oldS = ("" + ((float)i / (float) (i1*ampD)));
+					oldS = oldS.replace(".","");
+					while (oldS.startsWith("0")) oldS = oldS.substring(1);
+					String newS = (((char) 2) + "" + i + "/" + i1 + "" + ((char) 2));
+					if (out.contains(oldS)) {
+						if (oldS.length() > newS.length()) {
+							if ((oldStr+newStr).length() > (oldS+newS).length()) {
+								oldStr = oldS;
+								newStr = newS;
+								replaced = true;
+							}
+						}
+					}
+				}
+			}
+			if (!oldStr.equals("") && !newStr.equals("")) {
+				out = out.replace(oldStr, newStr);
+			}
+		}
+		return out;
+	}
+	
+	/**
+	 * decompress a string that has been compressed using the Multiplication compression
 	 *
 	 * @param toDecompress the string to decompress
 	 * @return the decompressed string
 	 */
-	public static String decompress(String toDecompress) {
+	public static String decompressMultiplication(String toDecompress) {
 		while (toDecompress.contains("" + ((char) 1))) {
 			String parse = toDecompress.substring(toDecompress.indexOf((char) 1) + 1);
 			parse = parse.substring(0, parse.indexOf((char) 1));
@@ -70,6 +111,62 @@ public class Numbers {
 			long result = amp(val1, val2);
 			toDecompress = toDecompress.replace(("" + ((char) 1)) + parse + ("" + ((char) 1)), "" + result);
 		}
+		return toDecompress;
+	}
+	
+	/**
+	 * decompress a string that has been compressed using the Division compression
+	 *
+	 * @param toDecompress the string to decompress
+	 * @return the decompressed string
+	 */
+	public static String decompressDivision(String toDecompress) {
+		while (toDecompress.contains("" + ((char) 2))) {
+			String parse = toDecompress.substring(toDecompress.indexOf((char) 2) + 1);
+			parse = parse.substring(0, parse.indexOf((char) 2));
+			String num1 = parse.substring(0, parse.indexOf("/"));
+			String num2 = parse.substring(parse.indexOf("/") + 1);
+			int val1 = Integer.parseInt(num1);
+			int val2 = Integer.parseInt(num2);
+			float result = ((float) val1 / (float) val2);
+			String res = ""+result;
+			res = res.replace(".","");
+			toDecompress = toDecompress.replace(("" + ((char) 2)) + parse + ("" + ((char) 2)), res);
+		}
+		return toDecompress;
+	}
+	
+	//Note to self: this should be oldest to newest
+	
+	/**
+	 * compress a string using optional number compression methods
+	 * if you don't want to use number compression, don't call this method at all, lol
+	 *
+	 * @param toCompress        the string to compress
+	 * @param useMultiplication whether or not it should use multiplication
+	 * @param useDivision       whether or not it should use division
+	 * @return the compressed string
+	 */
+	public static String compress(String toCompress, boolean useMultiplication, boolean useDivision) {
+		if (useMultiplication) toCompress = compressMultiplication(toCompress);
+		if (useDivision) toCompress = compressDivision(toCompress);
+		return toCompress;
+	}
+	
+	//Note to self: this should be newest to oldest
+	
+	/**
+	 * decompress a string, make sure you use the same series of true false as you used for compressing
+	 * if the string wasn't compressed with {@link Number} don't call this
+	 *
+	 * @param toDecompress      the string to decompress
+	 * @param useMultiplication whether or not it was compressed with multiplication
+	 * @param useDivision       whether or not it was compressed with division
+	 * @return the decompressed string
+	 */
+	public static String decompress(String toDecompress, boolean useMultiplication, boolean useDivision) {
+		if (useDivision) toDecompress = decompressDivision(toDecompress);
+		if (useMultiplication) toDecompress = decompressMultiplication(toDecompress);
 		return toDecompress;
 	}
 }
--- src/test/java/com/tfc/files/test/NumberCompressionTest.java
+++ src/test/java/com/tfc/files/test/NumberCompressionTest.java
@@ -4,11 +4,11 @@ import com.tfc.files.compression.Numbers;
 
 public class NumberCompressionTest {
 	public static void main(String[] args) {
-		String input = generate(1);
-//		String input = "164167681646592016449536164823041051852810399744103915521039564810379264103505921035468810326016";
-		String output = Numbers.compressMultiplication(input);
+//		String input = generate(1);
+		String input = "164167681646592016449536164823041051852810399744103915521039564810379264103505921035468810326016328497329432487632943248327498326473294712739127312532523423423412431241284781294761124671235123621123561246324623864923462312362194372149216498132646325792635212379218794267983265872693569327865923423217421946271896432148721984673975184351256712958172514871294791827321343724973294873294672394726666667";
+		String output = Numbers.compress(input,true,true);
 		System.out.println("Source:" + input);
-		System.out.println("Decompressed:" + Numbers.decompress(output));
+		System.out.println("Decompressed:" + Numbers.decompress(output,true,true));
 		System.out.println("Compressed:" + output);
 		System.out.println("Saved bytes:" + (input.length() - output.length()));
 	}
